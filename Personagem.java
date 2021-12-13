d347a009a5fe2d2fab4a5058cb5c4e288fd99bc2
H3rm4n0 <51340796+H3rm4n0@users.noreply.github.com>
2021-12-12 21:34:09 -0300

    agora tá certokkkjj (eu acho)

--- Personagem.java
+++ Personagem.java
@@ -1,5 +1,56 @@
 package projetin;
 
-public class Personagem {
+public abstract class Personagem {
+	
+	String nome;
+	double XP;
+	double mana;
+	int HP;
+	boolean attack;
+	
+	/* sei nem se as coisas aqui fazem sentidokkkkkk */
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+	public double getXP() {
+		return XP;
+	}
+	public void setXP(double XP) {
+		this.XP = XP;
+	}
+	
+	
+	
+	public double getMana() {
+		return mana;
+	}
+	public void setMana(double mana) {
+		this.mana = mana;
+	}
+	
+	
+	
+	public int getHP() {
+		return HP;
+	}
+	public void setHP(int HP) {
+		this.HP = HP;
+	}
+	
+	
+	
+	public boolean getAttack() {
+		return attack;
+	}
+	public void setAttack(boolean attack) {
+		this.attack = attack;
+	}
+	
 
-}
