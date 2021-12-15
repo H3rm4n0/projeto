488e563ba895a1d28442de8d1ca2878063cd6801
YheYinter <lu.m3105@gmail.com>
2021-12-14 21:10:16 -0300

    CRIAÇÃO DO CURANDEIRO

---
+++ Curandeiro.java
@@ -0,0 +1,32 @@
+package projetin;
+
+import java.util.Random;
+
+public class Curandeiro extends Personagem{
+
+    Random rand = new Random();
+    private boolean skill;
+
+    public Curandeiro(){
+        super();
+        this.setNome(getNome());
+        this.setHP(200);
+        this.setMana(0);
+    }
+
+
+    @Override
+    public void ataque(Personagem enemy){
+            this.setMana(this.getMana() + 5);
+            enemy.setHP(enemy.getHP() - (rand.nextInt(20 - 15) + 15));
+    }
+
+    @Override
+    public void ataqueEspecial(Personagem enemy){
+        if (this.getMana() >= 20){
+            this.setMana(this.getMana() - 20);
+            enemy.setHP(enemy.getHP() - (rand.nextInt(20 - 15) + 15));
+            this.setHP(this.getHP() + 50);
+        }
+    }
+}
