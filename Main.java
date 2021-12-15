1a667be4cc726f437023dfaa23ec4d6204064296
YheYinter <lu.m3105@gmail.com>
2021-12-14 22:00:46 -0300

    PEQUENAS ATUALIZAÇÕES

--- Main.java
+++ Main.java
@@ -1,27 +1,6 @@
 package projetin;
 
-import java.util.Objects;
-import java.util.Scanner;
-
 public class Main {
-	
-	static Scanner leitor = new Scanner(System.in);
-
-	public static void battle(Personagem user, Personagem enemy){
-		while (user.getHP() > 0  && enemy.getHP() > 0){
-			user.setAction(leitor.nextLine());
-			if (Objects.equals(user.getAction(), "ataque")){
-				user.ataque(enemy);
-			}else if (Objects.equals(user.getAction(), "ataque especial")) {
-				user.ataqueEspecial(enemy);
-			}
-			enemy.ataque(user);
-			if (enemy.getBurn()){
-				enemy.setHP(enemy.getHP() - 15);
-			}
-		}
-	}
-
 
 	public static void main(String[] args) {
 
